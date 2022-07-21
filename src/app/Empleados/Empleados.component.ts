b2392b19727c3680e270a3060261a7eadcde197c
DavidRodriguez <ndrr15@gmail.com>
2022-07-21 10:53:27 -0500

    unidad 5 vista y aplicada

---
+++ src/app/Empleados/Empleados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-empleados",
+    templateUrl: "./Empleados.component.html",
+    styleUrls: ["./Empleados.component.css"]
+})
+
+export class empleadosComponent {
+  title = 'firstAplication';
+  saludo = 'hola amiguitos';
+}
