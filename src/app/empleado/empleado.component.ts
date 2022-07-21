c455a56bb953a21ab4dae67db10528306d95c002
DavidRodriguez <ndrr15@gmail.com>
2022-07-21 11:55:35 -0500

    Sesion 6

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //template: "<p>Aquí iria un Empleado</p>",
   styleUrls: ['./empleado.component.css']
+  //styles:["p {background-color: red;}"]
 })
 export class EmpleadoComponent implements OnInit {
 
